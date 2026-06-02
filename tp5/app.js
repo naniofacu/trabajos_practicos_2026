59900780136e8d7b020c54237ddebc2475470a85
alumno <alumno@Instituto.local>
2026-06-02 09:47:46 -0300

    Facundo nanio

---
+++ tp5/app.js
@@ -0,0 +1,10 @@
+
+//declarar variable
+let numero1
+//asignar valor
+numero1=10
+//declarar y asignar al mismo tiempo
+let numero2=30
+numero1=5
+//mostrar la informacion
+console.log(numero1+numero1+numero2)
