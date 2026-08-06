13dd4e0f38a9da14c2062437dedb21822146d271
alumno <alumno@Instituto.local>
2026-08-06 10:39:41 -0300

    6/8

--- TP6/app.js
+++ TP6/app.js
@@ -1,4 +1,24 @@
-let boton1 = queryselector(body)
-let boton2 = queryselector()
-let boton3 = queryselector()
-let boton4 = queryselector()
+//ejercicio1
+let boton1 = document.querySelector("#btnfondo")
+let body = document.querySelector("body")
+
+boton1.onclick=function(){
+body.style.backgroundColor="blue"
+}
+
+//ejercicio2
+let boton2 = document.querySelector("#btntexto")
+let texto = document.querySelector("#parrafo")
+
+boton2.onclick=function(){
+    texto.textContent="este es el texto nuevo"
+}
+
+//ejercicio3
+let boton3 = document.querySelector("#btncolor")
+
+boton3.onclick=function(){
+texto.style.color = "red"
+}
+
+
